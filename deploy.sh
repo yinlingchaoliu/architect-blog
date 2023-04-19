ac25f1323d710d9296825062ad726b61bfbdceeb
陈桐 <tong.chen>
2023-04-19 13:44:41 +0800

    add site= https://yinlingchaoliu.github.io

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,15 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+push_addr="git@github.com:yinlingchaoliu/yinlingchaoliu.github.io.git"
 
-push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+# push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
+push_branch=master # 推送的分支
 
 # 生成静态文件
-npm run build
+yarn build
 
 # 进入生成的文件夹
 cd $dist_path
