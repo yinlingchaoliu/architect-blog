d790bd21cae72b0ef1653039250e5b2fdd85637a
陈桐 <tong.chen>
2023-05-04 14:17:12 +0800

    测试baiduPush

--- baiduPush.sh
+++ baiduPush.sh
@@ -3,6 +3,12 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+pwd
+
+echo `pwd`
+
+ls  -alrt
+
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://yinlingchaoliu.gitee.io&token=OdkhJGLdzCkQXjuc"
 
 # https://ziyuan.baidu.com/linksubmit/index
